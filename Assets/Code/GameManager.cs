03ab30324ba3ca0c302239647a2f6b5a979d696d
Þorgeir <thorgeir@radiantgames.is>
2017-01-31 00:26:34 +0000

    Added a very simple Main menu Screen

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -1,20 +1,44 @@
 ﻿using System.Collections;
 using System.Linq;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
     public DefenceSpawner defence;
     public AttackSpawner attack;
 
+    public RectTransform MainMenu;
+    public Animator infoBox;
+    private Text infoText;
+
+
     // Use this for initialization
-    IEnumerator Start () {
-        for (int i = 1; i < 5; i*=2)
+    void Start()
+    {
+        infoText = infoBox.GetComponentInChildren<Text>();
+
+    }
+    public void PlayPressed()
+    {
+        defence.gameObject.SetActive(true);
+        attack.gameObject.SetActive(true);
+        MainMenu.gameObject.SetActive(false);
+        StartCoroutine(StartGame());
+    }
+
+    IEnumerator StartGame()
+    {
+        for (int i = 1; i < 5; i++)
         {
-            yield return StartCoroutine(attack.StartRound(i, i + 10));
+            yield return StartCoroutine(attack.StartRound(i, i * 2 + 10));
             yield return new WaitWhile(() => FindObjectsOfType<Enemy>().Any());
+
+            infoBox.SetTrigger("Show");
+            infoText.text = "Round " + i;
+            yield return new WaitForSeconds(1f);
         }
     }
-    
+
 }
